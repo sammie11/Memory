325406077a88e0e2b07e8f4185896b92f937d507
Samuel GB <samuelgottfarb@gmail.com>
2023-09-22 21:59:42 +0200

    scafolding for grid view

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cards: [String]
+    let spacing: CGFloat
+    init(
+        cards: [String] = ["💩", "🥳", "💄", "💍", "🍔", "🥑", "🤺", "🚀"],
+        spacing: CGFloat = 20
+    ) {
+        self.cards = cards + cards
+        self.spacing = spacing
+    }
+    
     var body: some View {
-        HStack {
-            Image(systemName: "paperplane")
-                .resizable()
-                .frame(width: 40, height: 40)
-                .foregroundStyle(.red)
-            Text("Hello, europe!")
-                .font(.largeTitle)
+        GeometryReader { geometry in
+           
+            
+            let columns = (0..<4).map { _ in
+                GridItem(.flexible(minimum: 50, maximum: .infinity), spacing: spacing)
+            }
+            
+            LazyVGrid(
+                columns: columns,
+                spacing: spacing,
+                content: {
+                    ForEach(cards, id: \.self) { card in
+                        Button(card) {
+                            print("Tapped: \(card)")
+                        }
+                        .font(.largeTitle)
+                        .aspectRatio(1.0, contentMode: .fill)
+                        .frame(
+                            maxWidth: .infinity,
+                            minHeight: (geometry.size.height * 0.8) / (CGFloat(cards.count) / CGFloat(columns.count))
+                        )
+                        .background(Color.gray.opacity(0.5))
+                        .border(Color.black)
+                    }
+                }
+            )
+            .padding()
         }
-        .padding()
     }
 }
 
