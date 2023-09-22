b9591b377f1eb39e2305b29da9cc7e9bd1ba015f
Samuel Gottfarb <samuelgottfarb@Sajjode.local>
2023-09-22 19:05:42 +0200

    create new app

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Samuel Gottfarb on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
