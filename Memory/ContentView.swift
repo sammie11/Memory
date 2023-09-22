799c28803942fa5ecc53f22af2317886ebcc300a
Samuel GB <samuelgottfarb@gmail.com>
2023-09-22 19:47:30 +0200

    change appearance of content view

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Image(systemName: "paperplane")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundStyle(.red)
+            Text("Hello, europe!")
+                .font(.largeTitle)
         }
         .padding()
     }
