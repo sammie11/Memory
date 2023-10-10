7d247cd87a7e856b2f3355e3c91dbf943cc620a6
Alexander Cyon <alex.cyon@gmail.com>
2023-10-10 21:14:35 +0200

    play around with functions and tests

--- Memory/MemoryApp.swift
+++ Memory/MemoryApp.swift
@@ -15,3 +15,122 @@ struct MemoryApp: App {
         }
     }
 }
+
+struct Person {
+	
+	var weight: Double {
+		didSet {
+			updateRisks()
+		}
+	}
+	
+	private mutating func updateRisks() {
+		if bmi > 35 {
+			risks.insert(.disease(.diabetes))
+		}
+		if bmi > 40 {
+			risks.insert(.badEvent(.stroke))
+		}
+		print("updated risks")
+	}
+	
+	let height: Double
+	enum Risk: Hashable {
+		enum Disease: Hashable {
+			case diabetes
+		}
+		enum BadEvent: Hashable {
+			case stroke
+		}
+		case disease(Disease)
+		case badEvent(BadEvent)
+	}
+	var risks: Set<Risk>
+	init(weight: Double, height: Double, risks: Set<Risk> = []) {
+		self.weight = weight
+		self.height = height
+		self.risks = risks
+		updateRisks()
+	}
+	var bmi: Double {
+		weight / (height * height)
+	}
+}
+
+
+public struct Statistics {
+	public let min: Int
+	public let max: Int
+	public let sum: Int
+}
+
+func calculateStatistics1211(scores: [Int]) -> (min: Int, max: Int, sum: Int)? {
+	if scores.isEmpty { 
+//		return nil
+	}
+	var min = scores[0]
+	//...
+	fatalError()
+}
+
+func calculateStatistics432423(scores: [Int]) -> (min: Int, max: Int, sum: Int)? {
+	if !scores.isEmpty {
+		var min = scores[0]
+		var max = scores[0]
+		var sum = 0
+		
+		for score in scores {
+			if score > max {
+				max = score
+			} else if score < min {
+				min = score
+			}
+			sum += score
+		}
+
+
+		return (min, max, sum)
+	} else {
+		return nil
+	}
+}
+
+
+
+///
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int)? {
+//	guard !scores.isEmpty else {
+//		return nil
+//	}
+	guard let first = scores.first else { return nil }
+	
+	var min = first
+	var max = first
+	var sum = 0
+	
+	for score in scores {
+		if score > max {
+			max = score
+		} else if score < min {
+			min = score
+		}
+		sum += score
+	}
+
+
+	return (min, max, sum)
+}
+
+func test() {
+	calculateStatistics(scores: [])
+	
+	var ints = [5, 3, 100, 3, 9]
+	var firstint = ints.first
+	ints.insert(7, at: 0)
+	firstint = 1337
+	
+}
+
+func calculateStatistics2(scores: [Int]) -> Statistics {
+	fatalError()
+}
--- MemoryTests/MemoryTests.swift
+++ MemoryTests/MemoryTests.swift
@@ -6,30 +6,34 @@
 //
 
 import XCTest
+@testable import Memory
 
 final class MemoryTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	
+	func test_change_weight() {
+		struct Vector {
+			let bmi: Double
+			let expectedRisks: Set<Person.Risk>
+		}
+		func test(vector: Vector){
+			let height = 2.0
+			let weight = vector.bmi * height * height
+			let person = Person(weight: weight, height: height)
+			XCTAssertEqual(person.risks, vector.expectedRisks)
+			
+		}
+	
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+		let vectors: [Vector] = [
+			.init(bmi: 25, expectedRisks: []),
+			.init(bmi: 34, expectedRisks: []),
+			.init(bmi: 35.1, expectedRisks: [.disease(.diabetes)]),
+			.init(bmi: 36, expectedRisks: [.disease(.diabetes)]),
+			.init(bmi: 40.1, expectedRisks: [.disease(.diabetes), .badEvent(.stroke)]),
+			.init(bmi: 41, expectedRisks: [.disease(.diabetes), .badEvent(.stroke)]),
+		]
+		vectors.forEach(test(vector:))
+	}
+	
 }
